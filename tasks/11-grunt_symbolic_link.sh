19feb7c4b6bff42c52de41237dcb5eae75430344
Viacheslav Lotsmanov <lotsmanov89@gmail.com>
2015-01-08 16:40:23 +0300

    added checking for exists executable file in 11-grunt_symbolic_link.sh and 11-gulp_symbolic_link.sh

--- tasks/11-grunt_symbolic_link.sh
+++ tasks/11-grunt_symbolic_link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Version: r1
+# Version: r2
 # Author: Viacheslav Lotsmanov
 # License: GNU/GPLv3 (https://github.com/unclechu/web-front-end-deploy/blob/master/LICENSE)
 #
@@ -8,4 +8,13 @@ if [ -z "$PARENT_DEPLOY_SCRIPT" ]; then YOUR_SUBJECT="./_deploy/$(basename "$0")
 
 info_inline "Creating symbolic link to grunt-cli"
 rm ./grunt &>/dev/null
-if ln -s ./node_modules/.bin/grunt ./grunt &>/dev/null; then ok; else err; fi
+
+target='./node_modules/.bin/grunt'
+
+if [ ! -f "$target" ] || [ ! -x "$target" ]; then
+	err 1
+	info_err_clean "target file '$target' not found";
+	exit 1
+fi
+
+if ln -s "$target" ./grunt &>/dev/null; then ok; else err; fi
--- tasks/11-gulp_symbolic_link.sh
+++ tasks/11-gulp_symbolic_link.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Version: r1
+# Version: r2
 # Author: Viacheslav Lotsmanov
 # License: GNU/GPLv3 (https://github.com/unclechu/web-front-end-deploy/blob/master/LICENSE)
 #
@@ -8,4 +8,13 @@ if [ -z "$PARENT_DEPLOY_SCRIPT" ]; then YOUR_SUBJECT="./_deploy/$(basename "$0")
 
 info_inline "Creating symbolic link to gulp-cli"
 rm ./gulp &>/dev/null
-if ln -s ./node_modules/.bin/gulp ./gulp &>/dev/null; then ok; else err; fi
+
+target='./node_modules/.bin/gulp'
+
+if [ ! -f "$target" ] || [ ! -x "$target" ]; then
+	err 1
+	info_err_clean "target file '$target' not found";
+	exit 1
+fi
+
+if ln -s "$target" ./gulp &>/dev/null; then ok; else err; fi
